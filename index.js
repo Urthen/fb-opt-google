550d5ce415c6d4cedcf664431b25401e5eaf2043
Michael Pratt <urthen@gmail.com>
2015-04-16 11:56:41 -0500

    adding image search functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var querystring = require('querystring'),
     Entities = require('html-entities').AllHtmlEntities,
     request = require('request'),
+    _ = require('lodash'),
     util = require('util');
 
 var entities = new Entities();
@@ -36,6 +37,31 @@ function googleSearch(route, args) {
     }.bind(this));
 }
 
+function imageSearch(route, args, animated) {
+    var query = { v : '1.0', q : args.join(' '), rsz : '8', safe : 'active' };
+
+    if (animated) {
+        query.imgtype = 'animated';
+    }
+    var url = 'http://ajax.googleapis.com/ajax/services/search/images?' + querystring.stringify(query);
+
+    request.get(url, function (err, res, body) {
+        var results = JSON.parse(body),
+            data = results.responseData;
+
+        if (results && results.responseData && results.responseData.results && results.responseData.results.length > 0) {
+            var imageUrl = _.sample(data.results).unescapedUrl;
+            if (/(png|jpe?g|gif)/i.test(imageUrl.split('.').pop())) {
+                route.send(imageUrl);
+            } else {
+                route.send(imageUrl + '.png');
+            }
+        } else {
+            route.send('?google_no_results');
+        }
+    });
+}
+
 module.exports = {
     displayname : 'Google',
     description : 'Let me google that for you.',
@@ -43,9 +69,27 @@ module.exports = {
     commands : [{
             name : 'Google',
             description : 'Finds the first search result. Use the "more" option to get more results.',
-            usage : 'google [more ](search terms)',
+            usage : 'google [more] (search terms)',
             trigger : /google/i,
             func : googleSearch
+        },
+        {
+            name : 'Image Search',
+            description : 'Searches for images for your query, and returns a random result.',
+            usage : 'image [me] (search terms)',
+            trigger : /image( me)?/i,
+            func : function (route, args) {
+                imageSearch(route, args, false);
+            }
+        },
+        {
+            name : 'Animated Image Search',
+            description : 'Searches for animated images for your query, and returns a random result.',
+            usage : 'animate [me] (search terms)',
+            trigger : /animate( me)?/i,
+            func : function (route, args) {
+                imageSearch(route, args, true);
+            }
         }
     ]
 };
